10cfa25c6f5c4ffae0f9ed260eecb256e708dd6a
Gonzalo <gmmolina92@gmail.com>
2021-12-08 13:27:13 -0300

    Copy code

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using VirtualHosp.Context;
+using VirtualHosp.Models;
+
+namespace VirtualHosp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly HospitalDbContext _context;
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(HospitalDbContext context, ILogger<HomeController> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var consultas = await _context.Consultas.ToListAsync();
+            foreach (var consulta in consultas)
+            {
+                if (consulta.MedicoId != 0)
+                {
+                    consulta.Medico = await _context.Medicos.FindAsync(consulta.MedicoId);
+                }
+                if (consulta.PacienteId != 0)
+                {
+                    consulta.Paciente = await _context.Pacientes.FindAsync(consulta.PacienteId);
+                }
+            }
+            return View(consultas);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/Usuario.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using VirtualHosp.Models.Enums;
+
+namespace VirtualHosp.Models
+{
+    public class Usuario
+    {
+        [Required(ErrorMessage = "Nombre es requerido")]
+        public string Nombre { get; set; }
+        [Required(ErrorMessage = "Apellido es requerido")]
+        public string Apellido { get; set; }
+        [Required(ErrorMessage = "Fecha es requerida")]
+        [Display(Name = "Fecha de nacimiento")]
+        [DataType(DataType.Date)]
+        public DateTime FechaNacimiento { get; set; }
+        [Required(ErrorMessage = "Tipo es requerido")]
+        [Display(Name = "Tipo de documento")]
+        public TipoDocumento TipoDocumento { get; set; }
+        [Required(ErrorMessage = "Numero es requerido")]
+        [Display(Name = "Número de documento")]
+        [Range(0.0, double.MaxValue, ErrorMessage = "El campo {0} debe ser mayor a {1}.")]
+        public int NumeroDocumento { get; set; }
+        [Display(Name = "Estado civil")]
+        public EstadoCivil? EstadoCivil { get; set; }
+        [Required(ErrorMessage = "Email es requerido")]
+        [EmailAddress]
+        public string Email { get; set; }
+        public string Direccion { get; set; }
+        public string Nacionalidad { get; set; }
+        public string Ciudad { get; set; }
+        [Display(Name = "Codigo Postal")]
+        public int? CodigoPostal { get; set; }
+        [DataType(DataType.Password)]
+        [Display(Name = "Contraseña")]
+        [Required(ErrorMessage = "Contraseña es requerida")]
+        [StringLength(30, ErrorMessage = "Debe ser entre 5 y 30 caracteres", MinimumLength = 5)]
+        public string Password { get; set; }
+        public string NombreCompleto => Apellido + ", " + Nombre;
+    }
+}
